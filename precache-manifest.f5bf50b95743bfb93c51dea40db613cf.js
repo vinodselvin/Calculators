204b79cdb6699361f00ac05c0c3d60aa5ae964f8
Vinod Selvin <vinod.selvin@switchme.in>
2020-10-22 20:33:58 +0530

    deploy

---
+++ precache-manifest.f5bf50b95743bfb93c51dea40db613cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2343467db5a3b02cfb9ae9bcf94119",
+    "url": "/Calculators/index.html"
+  },
+  {
+    "revision": "8e6babb68334775377bd",
+    "url": "/Calculators/static/css/main.8e7fe575.chunk.css"
+  },
+  {
+    "revision": "766b5e958cf82ea38ca0",
+    "url": "/Calculators/static/js/2.6b32a0f9.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Calculators/static/js/2.6b32a0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e6babb68334775377bd",
+    "url": "/Calculators/static/js/main.f6d37d90.chunk.js"
+  },
+  {
+    "revision": "ac594ec236a7822b7d5c",
+    "url": "/Calculators/static/js/runtime-main.180b7a65.js"
+  },
+  {
+    "revision": "37e139b72b009f51b701c9f1fc2a5ccc",
+    "url": "/Calculators/static/media/logo.37e139b7.svg"
+  }
+]);
